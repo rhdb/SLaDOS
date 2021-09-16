16abd255fbd7dd5d33a54aadbd6bd4a08e680c0c
IsaccBarker <plutoisaplanet4324@gmail.com>
2021-09-15 21:04:05 -0600

    checking in for the night, after spending hours ricing my vim

---
+++ src/dispatch.rs
@@ -0,0 +1,37 @@
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use log::{info, error};
+
+// #[tokio::main]
+pub async fn powered_dispatch(bind_on: String) -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind(&bind_on).await?;
+    info!("Binding on {}", bind_on);
+
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        error!("Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    error!("Failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
-}
+pub mod dispatch;
+
+use clap::{Arg, App, SubCommand};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let matches = App::new("SLaDOS")
+        .version("1.0.0")
+        .author("Milo Banks <milobanks@rowlandhall.org>, Eli Hatton <elihatton@rowlandhall.org>")
+        .about("Standardised Logging and Decentralized student-Observation System.")
+        .arg(Arg::with_name("config")
+            .long("config")
+            .value_name("FILE")
+            .help("Sets a custom config file")
+            .takes_value(true))
+        .subcommand(SubCommand::with_name("kiosk")
+            .about("Run as a kiosk. Pretty much the only option.")
+            .subcommand(SubCommand::with_name("registration")
+                .about("A registration kiosk."))
+            .subcommand(SubCommand::with_name("checkpoint")
+                .about("A kiosk to keep track of attendance. A checkpoint."))
+            )
+        .get_matches();
+
+    dispatch::powered_dispatch("127.0.0.1:8000".to_owned()).await
+} 
